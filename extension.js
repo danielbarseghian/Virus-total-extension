c6a5f0ef55866e66bd3353b5c7747766f3665839
Daniel BARSEGHIAN <barseghian.daniel@gmail.com>
2026-07-23 23:18:16 +0200

    full backend works!

--- extension.js
+++ extension.js
@@ -1,26 +1,46 @@
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-    if (changeInfo.status !== "complete" || !tab.url) return;
+  if (changeInfo.status !== "complete" || !tab.url) return;
 
-    const { apiKey } = await chrome.storage.local.get("apiKey");
+  const { apiKey } = await chrome.storage.local.get("apiKey");
+  if (!apiKey) return;
 
-    if (!apiKey) return;
+  const domain = new URL(tab.url).hostname;
 
-    const domain = new URL(tab.url).hostname;
-
-    console.log(domain);
-
-    const options = {
-    method: 'POST',
+  const options = {
+    method: 'GET',
     headers: {
-        accept: 'application/json',
-        'content-type': 'application/x-www-form-urlencoded',
-        "x-apikey": apiKey
+      accept: 'application/json',
+      'x-apikey': apiKey
     }
-    };
+  };
 
-    var res = fetch(`https://www.virustotal.com/api/v3/urls/${domain}`, options)
+  console.log(`checking on ${domain}`);
+
+  fetch(`https://www.virustotal.com/api/v3/domains/${domain}`, options)
     .then(res => res.json())
-    .then(res => console.log(res))
+    .then(json => {
+      const results = json.data.attributes.last_analysis_results;
+      let harmful = 0;
+      let total = 0;
+
+      for (const [engine, result] of Object.entries(results)) {
+        total += 1;
+        if (result.category === "malicious") {
+          harmful += 1;
+        }
+      }
+
+      console.log(`Malicious: ${harmful} total: ${total}`);
+
+      if (harmful > 0 && harmful < 4) {
+        console.log("YELLOW");
+      } 
+      else if (harmful >= 4) {
+        console.log("RED");
+      }
+      else {
+        console.log("GREEN GIANT");
+      }
+    })
     .catch(err => console.error(err));
 });
-
